21bec54d8343eb0aaa6d7c92317427285490de68
bender <kudin.dima@gmail.com>
2026-03-06 00:06:03 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function LandingPage() {
       <div id="nav" data-section="nav">
         <NavbarStyleApple
           brandName="Taishan"
-          logo="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AXh5KliD4l7rWjrQZkOq7ZuPnH/uploaded-1772755165036-dookr298.png"
+          logoSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AXh5KliD4l7rWjrQZkOq7ZuPnH/uploaded-1772755165036-dookr298.png"
           navItems={[
             { name: "Services", id: "services" },
             { name: "Gallery", id: "gallery" },
